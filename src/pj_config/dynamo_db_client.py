014365aab7e01942abbd0b23dc76bb9593b4a5d8
cydiatsang <cydia135260@gamil.com>
2024-01-14 21:51:10 +0800

    update project.ipynb and update README.md

--- src/pj_config/dynamo_db_client.py
+++ src/pj_config/dynamo_db_client.py
@@ -3,7 +3,7 @@ import boto3
 def get_dynamo_db_client():
     # Replace 'your_access_key', 'your_secret_key', and 'your_region' with your actual AWS credentials and region
     aws_access_key = 'AKIA45YHIOUEL552LTSU'
-    aws_secret_key = '7TvJooiPPitNepeSMiNofOaASyx/NU2h+/8a1kgA'
+    aws_secret_key = ''
     aws_region = 'us-east-1'
 
     # Create a DynamoDB client
