cfa768dfb0bf7fbc98ed3f68061d4b10dd5ed905
cydiatsang <cydia135260@gamil.com>
2024-02-16 09:38:19 +0800

    update project.ipynb

--- src/pj_config/dynamo_db_client.py
+++ src/pj_config/dynamo_db_client.py
@@ -1,9 +1,32 @@
 import boto3
+from cryptography.fernet import Fernet
+
+def encrypt_access_key():
+    key = Fernet.generate_key()
+    with open("key.txt", "wb") as file:
+        file.write(key)
+    cipher_suite = Fernet(key)
+
+    aws_access_key = cipher_suite.encrypt('AKIA45YHIOUEL552LTSU'.encode())
+    with open("aws_access_key.txt", "wb") as file:
+        file.write(aws_access_key)
+
+    aws_secret_key = cipher_suite.encrypt('7TvJooiPPitNepeSMiNofOaASyx/NU2h+/8a1kgA'.encode())
+    with open("aws_secret_key.txt", "wb") as file:
+        file.write(aws_secret_key)
+
 
 def get_dynamo_db_client():
-    # Replace 'your_access_key', 'your_secret_key', and 'your_region' with your actual AWS credentials and region
-    aws_access_key = ''
-    aws_secret_key = ''
+    with open("key.txt", "rb") as file:
+        key = file.read()
+    cipher_suite = Fernet(key)
+    with open("aws_access_key.txt", "rb") as file:
+        aws_access_key_encrypted_string = file.read()
+
+    aws_access_key = cipher_suite.decrypt(aws_access_key_encrypted_string).decode()
+    with open("aws_secret_key.txt", "rb") as file:
+        aws_secret_key_encrypted_string = file.read()
+    aws_secret_key = cipher_suite.decrypt(aws_secret_key_encrypted_string).decode()
     aws_region = 'us-east-1'
 
     # Create a DynamoDB client
